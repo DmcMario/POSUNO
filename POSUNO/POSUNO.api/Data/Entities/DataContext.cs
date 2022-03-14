02c15e64afe7379fb74f31ff8f9ff89a84677183
DmcMario <mario.gene@hotmail.com>
2022-03-14 15:02:45 -0500

    API Create

---
+++ POSUNO/POSUNO.api/Data/Entities/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace POSUNO.api.Data.Entities
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasIndex(p => p.Name).IsUnique();
+            modelBuilder.Entity<User>().HasIndex(p => p.Email).IsUnique();
+        }
+    }
+}
